4fa4f6cf497f92894cf781cf11bfd9d19e16282a
Hg <30391617+teehg@users.noreply.github.com>
2019-04-01 22:30:11 +0800

    Update redis_subscriber.py

--- redis_subscriber.py
+++ redis_subscriber.py
@@ -21,7 +21,7 @@ class Listener(threading.Thread):
         self.pubsub.subscribe(channels)
         self.hr_dataset = []
         self.spo2_dataset = []
-
+        
     def work(self, item):
         try:
             data_from_queue = json.loads(item['data'].decode('utf-8'))
@@ -30,12 +30,12 @@ class Listener(threading.Thread):
             if item['channel'].decode("utf-8") == 'heart_rate':
                 self.hr_dataset.append(data_from_queue)
                 # Sending data in chunks of 100 data points per API request
-                if len(self.hr_dataset) >= 100:
+                if len(self.hr_dataset) >= 20:
                     try:
-                        headers = {'data_type': 'heart_rate', 'device_authorization': 'easy_nmr_129'}
+                        headers = {'data_type': 'heart_rate'}
                         response = requests.post('http://35.240.193.146:5010/api/stats/new', json=self.hr_dataset,
                                                  headers=headers)
-                        print('Response code: ' + response.status_code)
+                        print('Response code: ' + str(response.status_code))
                         if response.status_code != 200:
                             for i in self.hr_dataset:
                                 r.publish('heart_rate', i)
@@ -53,12 +53,12 @@ class Listener(threading.Thread):
             elif item['channel'].decode("utf-8") == 'spo2':
                 self.spo2_dataset.append(data_from_queue)
                 # Sending data in chunks of 100 data points per API request
-                if len(self.spo2_dataset) >= 100:
+                if len(self.spo2_dataset) >= 20:
                     try:
-                        headers = {'data_type': 'spo2', 'device_authorization': 'easy_nmr_129'}
+                        headers = {'data_type': 'spo2'}
                         response = requests.post('http://35.240.193.146:5010/api/stats/new', json=self.spo2_dataset,
                                                  headers=headers)
-                        print('Response code: ' + response.status_code)
+                        print('Response code: ' + str(response.status_code))
                         if response.status_code != 200:
                             for i in self.spo2_dataset:
                                 r.publish('spo2', i)
@@ -77,16 +77,13 @@ class Listener(threading.Thread):
             print(e)
 
     def run(self):
+        i = 0
         for item in self.pubsub.listen():
             self.work(item)
-        if self.hr_dataset:
-            headers = {'data_type': 'heart_rate'}
-            requests.post('http://35.240.193.146:5010/api/stats/new', json=self.hr_dataset,
-                          headers=headers)
-        if self.hr_dataset:
-            headers = {'data_type': 'spo2'}
-            requests.post('http://35.240.193.146:5010/api/stats/new', json=self.spo2_dataset,
-                          headers=headers)
+            i+=1
+        print("All data in queue are clear")
+        print(i)
+        
 
 
 if __name__ == "__main__":
