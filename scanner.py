ea36a33c347fa0c5ba7e47dca412bb308b5e2b1e
xMinh129 <19287692+xMinh129@users.noreply.github.com>
2019-05-14 09:42:32 +0700

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -74,7 +74,7 @@ def create_new_data_session(device_id):
     headers = {"Authorization": DEVICE_AUTH_CODE}
 
     # Create a new session
-    r = requests.post('http://35.247.135.116:5010/api/sessions/new', json=session_info, headers=headers)
+    r = requests.post('http://35.240.233.234:5010/api/sessions/new', json=session_info, headers=headers)
     print(r)
     return session_id
 
